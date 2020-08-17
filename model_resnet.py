2ec6a489e09d2c25ba6f8d6427dd040324acf4e3
Btrox148 <marioalberto9741@gmail.com>
2020-08-16 23:31:28 -0500

    Fix libraries

--- model_resnet.py
+++ model_resnet.py
@@ -35,3 +35,4 @@ valid_dataset = Dataset(
 
 train_loader = DataLoader(train_dataset, batch_size=8, shuffle=True, num_workers=12)
 valid_loader = DataLoader(valid_dataset, batch_size=1, shuffle=False, num_workers=4)
+
