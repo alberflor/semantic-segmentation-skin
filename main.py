f5d9b26d8521157042e23d8fd995f7491e42b8c6
Btrox148 <marioalberto9741@gmail.com>
2020-08-18 22:24:02 -0500

    Using CUDA

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
 import os
 import load_dataset
 
-os.environ['CUDA_VISIBLE_DEVICES'] = '0'
-
 DATA_DIR = "Data/"
 train_img = os.path.join(DATA_DIR,"Training_input")
 train_mask = os.path.join(DATA_DIR,"Training_annot")
 
 dataset = load_dataset.Dataset(train_img, train_mask, classes=['melanoma'])
-image, mask = dataset[1]
+image, mask = dataset[3]
 
 load_dataset.visualize(image= image, mask = mask)
 
