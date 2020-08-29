0e679ea1face1f677b4b90ce3c03e5678e3a5b45
Btrox148 <marioalberto9741@gmail.com>
2020-08-28 21:00:43 -0500

    Fix conflict in VENV lib installations

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 import os
 import load_dataset
+import torch
+import numpy as np
+import segmentation_models_pytorch as smp
+
+# Based on https://github.com/qubvel/segmentation_models.pytorch tutorial
 
 DATA_DIR = "Data/"
 train_img = os.path.join(DATA_DIR,"Training_input")
 train_mask = os.path.join(DATA_DIR,"Training_annot")
 
 dataset = load_dataset.Dataset(train_img, train_mask, classes=['melanoma'])
-image, mask = dataset[3]
+image, mask = dataset[5]
 
-load_dataset.visualize(image= image, mask = mask)
+load_dataset.visualize(image= image, mask = mask.squeeze())
 
+model = smp.FPN('resnet34', in_channels=1)
