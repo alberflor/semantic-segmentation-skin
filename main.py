705bd6f165b821d9b0a52c7ac246ad13692e502f
Btrox148 <marioalberto9741@gmail.com>
2020-08-30 23:52:06 -0500

    Paretto rule: 80/20 for train/test on dataset

--- main.py
+++ main.py
@@ -4,9 +4,11 @@ import torch
 import numpy as np
 import segmentation_models_pytorch as smp
 
-# Based on https://github.com/qubvel/segmentation_models.pytorch tutorial
+# Based on https://github.com/qubvel/segmentation_models.pytorch/blob/master/examples/cars%20segmentation%20(camvid).ipynb tutorial
 
+CLASSES = ['melanoma']
 DATA_DIR = "Data/"
+
 train_img = os.path.join(DATA_DIR,"Training_input")
 train_mask = os.path.join(DATA_DIR,"Training_annot")
 
@@ -15,4 +17,13 @@ image, mask = dataset[5]
 
 load_dataset.visualize(image= image, mask = mask.squeeze())
 
-model = smp.FPN('resnet34', in_channels=1)
+#Model creation
+model = smp.FPN(encoder_name='se_resnet50_32x4d', encoder_weights='imagenet', classes= 1, activation= 'relu')
+preprocessing_fn = smp.encoders.get_preprocessing_fn('se_resnet50_32x4d', 'imagenet')
+
+#Training
+training_dataset = load_dataset.Dataset(train_img, train_mask, classes=CLASSES)
+validation_dataset = load_dataset.Dataset(train_img, train_mask, classes=CLASSES)
+
+#validation_dataset = Dataset()
+
