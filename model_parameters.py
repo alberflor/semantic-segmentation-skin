0c1a7a3d3f32cedb3e121e5320b8b5e40df3f963
Alberto Flores <marioalberto9741@gmail.com>
2020-10-13 22:18:56 -0500

    class params wrapped, fix auxiliar modules

--- model_parameters.py
+++ model_parameters.py
@@ -1,7 +1,9 @@
 # This module class wraps all of the model parameters.
 
 import os
+import torch
 import segmentation_models_pytorch as smp
+
 class model_params:
 
     #model_params
@@ -15,17 +17,13 @@ class model_params:
     data = 'Data/'
     model_dir = 'Model/'
     file_name = 'se_resnext524d.pth'
-    model_path = model_dir + filename
+    model_path = model_dir + file_name
     test_dir = 'Data/Test_images'
-
     images_dir = os.path.join(data,"Training_input")
     masks_dir = os.path.join(data, "Training_annot")
 
+    #model_config
     model = smp.FPN(encoder_name=encoder, encoder_weights=pre_tr_weights, classes=len(classes), activation=act_func)
     loss = smp.utils.losses.DiceLoss()
-    
-
-    #run_params
-    train_new_model = False
-    test_random_sample = True
-    
+    metrics = [smp.utils.metrics.IoU(threshold=0.5),]
+    optimizer = torch.optim.Adam([dict(params=model.parameters(),lr=0.0001),])    
